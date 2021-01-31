0437e195e4408d977ee4561049acc25cfaa4cf4a
ckanzafluks <ckanza@fluksaqua.com>
2021-01-31 22:15:30 +0100

    api documentation

--- config/bundles.php
+++ config/bundles.php
@@ -26,4 +26,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
